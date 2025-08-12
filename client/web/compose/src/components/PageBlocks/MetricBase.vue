6ea25d8baf4b790012c7091fd94f199a4aff3076
JoaoFigueiredoTechbase <joaofigueiredowp@gmail.com>
2025-08-12 11:14:18 +0100

    spinner animation removed

--- client/web/compose/src/components/PageBlocks/MetricBase.vue
+++ client/web/compose/src/components/PageBlocks/MetricBase.vue
@@ -4,7 +4,7 @@
     v-on="$listeners"
     @refreshBlock="refresh"
   >
-    <div
+    <!-- <div
       v-if="isProcessing"
       class="d-flex align-items-center justify-content-center h-100"
     >
@@ -37,7 +37,42 @@
           />
         </div>
       </div>
+    </template> -->
+
+    <div
+    class="metric-wrapper h-100 position-relative"
+    :class="{ 'is-loading': processing }"
+  >
+    <!-- keep the value visible -->
+    <template v-if="!error">
+      <div
+        v-for="(m, mi) in options.metrics"
+        :key="mi"
+        class="d-flex align-items-center justify-content-center overflow-hidden h-100"
+      >
+        <div
+          v-for="(v, i) in formatResponse(m, mi)"
+          :key="i"
+          class="w-100 h-100 px-2 py-1"
+          :class="m.drillDown.enabled ? 'pointer' : ''"
+          @click="drillDown(m, mi)"
+        >
+          <metric-item
+            :metric="m"
+            :value="v"
+          />
+        </div>
+      </div>
     </template>
+
+    <label
+      v-else
+      class="text-primary p-3"
+    >
+      {{ error }}
+    </label>
+  </div>
+
   </wrap>
 </template>
 
